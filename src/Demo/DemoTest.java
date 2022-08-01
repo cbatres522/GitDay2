924150576aa1be83c2485ae9293b93347584883c
lee_b <lee_b@DESKTOP-BNP654P>
2022-08-01 18:31:46 -0500

    Implemented user456

--- src/Demo/DemoTest.java
+++ src/Demo/DemoTest.java
@@ -11,4 +11,9 @@ public class DemoTest {
 	public static void add(int a, int b) {
 		System.out.println(a+b);
 	}
+	
+	//User456
+	public static void divide(int a, int b) {
+		System.out.println(a/b);
+	}
 }
